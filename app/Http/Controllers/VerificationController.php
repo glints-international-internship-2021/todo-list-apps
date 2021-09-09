0e8a458f7f13c85b294269fe744be3bfb282eb54
wkwkwill <willykomara@gmail.com>
2021-09-09 07:05:31 +0700

    Updated VerificationController
    - Added rules for response 401 "token expired" by checking verification_token's field [is_active] and [expired_at]
    - Fixed response 404 status to "failed"
    - Fixed token matching rule

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -22,20 +22,38 @@ class VerificationController extends Controller
         $token = $request->token;
 
         $tokenRecord = DB::table('verification_tokens')
-                        ->where('token', base64_encode($token))
+                        ->where('token', $token)
                         ->first();
+        
         if (!!$tokenRecord) {
             // If token exist in the database
-            $code = 200;
-            $status = "success";
-            $message = "verification success";
-            return response()->json(compact('code', 'status', 'message'));
+
+            // Token is active if is_active = true AND current time is less than expiration time
+            $isTokenActive = $tokenRecord->is_active && (Carbon::now()->lt($tokenRecord->expired_at));
+            
+            if ($isTokenActive) {
+                // If token not expired/used yet
+                $code = 200;
+                $status = "success";
+                $message = "verification success";
+
+                return response()->json(compact('code', 'status', 'message'));
+            } else {
+                // If token expired or already used
+                $code = 401;
+                $status = "failed";
+                $message = "verification failed, token is already expired or used.";
+
+                return response()->json(compact('code', 'status', 'message'));
+            }
+            
         } else {
             
             // If token does not exist in the database
             $code = 404;
-            $status = "success";
+            $status = "failed";
             $message = "verification failed, invalid token.";
+
             return response()->json(compact('code', 'status', 'message'));
         }
     }
